b686aac2c5684aa72ac1c8a8f0dcf7182ffa235c
okuda hiroko <okuda_hiroko@hotmail.com>
2020-06-27 11:50:40 +0000

    No.30

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,23 +9,24 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
-                                <div class="title p-2">
-                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                <div class="name p-2">
+                                    <h1>{{ str_limit($headline->name, 70) }}</h1>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
+                            <p class="gender mx-auto">{{ str_limit($headline->gender, 70) }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="hobby mx-auto">{{ str_limit($headline->hobby, 70) }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="introduction">{{ str_limit($headline->introduction, 70) }}</p>
                         </div>
                     </div>
                 </div>
             </div>
-        @endif 
+        @endif
         <hr color="#c0c0c0">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
@@ -36,17 +37,18 @@
                                 <div class="date">
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
-                                <div class="title">
-                                    {{ str_limit($post->title, 150) }}
+                                <div class="name">
+                                    {{ str_limit($post->name, 150) }}
                                 </div>
-                                <div class="body mt-3">
-                                    {{ str_limit($post->body, 1500) }}
+                                <div class="gender">
+                                    {{ str_limit($post->gender, 150) }}
                                 </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif    
+                                <div class="hobby">
+                                    {{ str_limit($post->hobby, 150) }}
+                                </div>
+                                <div class="introduction">
+                                    {{ str_limit($post->introduction, 150) }}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -55,5 +57,4 @@
             </div>
         </div>
     </div>
-    </div>
-@endsection    
+    @endsection
